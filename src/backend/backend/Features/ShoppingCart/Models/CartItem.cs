10bf911138c07b2b71a23480136bcf6f80520543
Systemad <dannesuke@gmail.com>
2023-01-06 16:48:22 +0100

    Cleanup

--- src/backend/backend/Features/ShoppingCart/Models/CartItem.cs
+++
@@ -1,6 +0,0 @@
-﻿using backend.Features.Product.Models;
-
-namespace backend.Features.ShoppingCart.Models;
-
-[GenerateSerializer, Immutable]
-public record CartItem(string UserId, int Quantity, ProductDetail ProductDetail);
